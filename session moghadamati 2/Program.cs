5fc20a3c164dd8145d8e1645344429260b18e28b
ryhanearefi <r_arefy@yahoo.com>
2023-07-08 07:54:37 +0330

    message

---
+++ session moghadamati 2/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    string[] Student = new string[3];
+    int[] Nomre_Student = new int[3];
+
+
+    for (int i = 0; i < Student.Length; i++)
+    {
+      Console.Write("Plz.Enter A Fname  Student : ");
+      Student[i] = System.Console.ReadLine();
+
+    }
+
+
+
+    for (int j = 0; j < Nomre_Student.Length; j++)
+    {
+      Console.Write("Plz.Enter A Fname  Student : ");
+      Nomre_Student[j] = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine(" ** Show Fname & Mark Student ***");
+    for (int i = 0; i < Nomre_Student.Length; i++)
+    {
+      Console.WriteLine("Fname: " + Nomre_Student[i] + " -----> " + " Mark : " + Nomre_Student[i]);
+    }
+  }
+}
